05137e725f7184a5229e76f92cc3a3e51668a70f
guohailong <1105744725@qq.com>
2026-09-05 22:37:02 +0800

    启用 R8 与资源压缩精简 release 包体积

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             if (hasReleaseSigning) {
                 signingConfig = signingConfigs.getByName("release")
             }
--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,1 +1,5 @@
+# 保留崩溃堆栈的可读性行号,上报时可还原混淆映射
+-keepattributes SourceFile,LineNumberTable
+-renamesourcefileattribute SourceFile
 
+# okhttp / coroutines / compose / androidx 均自带 consumer rules,无需额外 keep
